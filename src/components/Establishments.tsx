c517a5d9d1dc860f6e241746a0d80512fb2a25dd
Rizvan <rabota_turkey@bk.ru>
2023-03-09 12:36:39 +0300

    k

---
+++ src/components/Establishments.tsx
@@ -0,0 +1,8 @@
+
+export const Establishments = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import '../css/styles.css'
 
 export const HomePage = () => {
     return(
-        <div className='wrapper'>
+        <div className='main-unit'>
             <div className='green-block-and-text'>
                 <div className='green-block'></div>
                 <div>
